8580cc96ab83afa0c648fbbf3697514fe3cc07b2
zezef510 <zezefff510@gmail.com>
2023-05-15 13:58:18 +0700

    b1

---
+++ test1.js
@@ -0,0 +1,15 @@
+function lastIndexOf(arr, elt, start = arr.length-1) {
+
+    for (let i = start ; i >= 0; i--)
+
+
+        if (arr[i] === elt) return i
+
+
+    return -1
+
+
+}
+
+
+console.log(lastIndexOf([1, 2, 1, 2], 2))
